a0ae504d43cd2c608e7f5ef34f859a92d81b82cd
Jareth McGhee <sinjur.j@gmail.com>
2024-10-15 22:32:04 -0500

    implement display of single structuredgrid file

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,61 @@
-#include <vtkActor.h>
-#include <vtkCamera.h>
-#include <vtkCylinderSource.h>
+#include <vtkColorTransferFunction.h>
 #include <vtkNamedColors.h>
-#include <vtkNew.h>
 #include <vtkPolyDataMapper.h>
-#include <vtkProperty.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
 #include <vtkRenderer.h>
+#include <vtkStructuredGrid.h>
+#include <vtkStructuredGridGeometryFilter.h>
+#include <vtkStructuredGridReader.h>
 
-#include <array>
-
-int main(int, char *[]) {
+int main(int argc, char *argv[]) {
     vtkNew<vtkNamedColors> colors;
 
-    // Set the background color.
-    std::array<unsigned char, 4> bkg{{26, 51, 102, 255}};
-    colors->SetColor("BkgColor", bkg.data());
+    // Verify input arguments
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0]
+                  << " Filename(.vtk) e.g. SampleStructGrid.vtk" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    // This creates a polygonal cylinder model with eight circumferential facets
-    // (i.e, in practice an octagonal prism).
-    vtkNew<vtkCylinderSource> cylinder;
-    cylinder->SetResolution(8);
+    std::string inputFilename = argv[1];
 
-    // The mapper is responsible for pushing the geometry into the graphics
-    // library. It may also do color mapping, if scalars or other attributes are
-    // defined.
-    vtkNew<vtkPolyDataMapper> cylinderMapper;
-    cylinderMapper->SetInputConnection(cylinder->GetOutputPort());
+    // Read the file
+    vtkNew<vtkStructuredGridReader> reader;
+    reader->SetFileName(inputFilename.c_str());
+    reader->Update();
 
-    // The actor is a grouping mechanism: besides the geometry (mapper), it
-    // also has a property, transformation matrix, and/or texture map.
-    // Here we set its color and rotate it around the X and Y axes.
-    vtkNew<vtkActor> cylinderActor;
-    cylinderActor->SetMapper(cylinderMapper);
-    cylinderActor->GetProperty()->SetColor(
-        colors->GetColor4d("Tomato").GetData());
-    cylinderActor->RotateX(30.0);
-    cylinderActor->RotateY(-45.0);
+    vtkNew<vtkStructuredGridGeometryFilter> geometryFilter;
+    geometryFilter->SetInputConnection(reader->GetOutputPort());
+    geometryFilter->Update();
 
-    // The renderer generates the image
-    // which is then displayed on the render window.
-    // It can be thought of as a scene to which the actor is added
+    std::array<double, 2> scalarRange;
+    reader->GetOutput()->GetScalarRange(scalarRange.data());
+
+    vtkNew<vtkColorTransferFunction> ctf;
+    ctf->AddRGBPoint(scalarRange[0], 0.0, 0.0, 1.0);
+    ctf->AddRGBPoint((scalarRange[0] + scalarRange[1]) / 2.0, 1.0, 1.0, 1.0);
+    ctf->AddRGBPoint(scalarRange[1], 1.0, 0.0, 0.0);
+
+    // Visualize
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputConnection(geometryFilter->GetOutputPort());
+    mapper->SetLookupTable(ctf);
+
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper);
+
     vtkNew<vtkRenderer> renderer;
-    renderer->AddActor(cylinderActor);
-    renderer->SetBackground(colors->GetColor3d("BkgColor").GetData());
-    // Zoom in a little by accessing the camera and invoking its "Zoom" method.
-    renderer->ResetCamera();
-    renderer->GetActiveCamera()->Zoom(1.5);
-
-    // The render window is the actual GUI window
-    // that appears on the computer screen
     vtkNew<vtkRenderWindow> renderWindow;
-    renderWindow->SetSize(300, 300);
     renderWindow->AddRenderer(renderer);
-    renderWindow->SetWindowName("Cylinder");
+    renderWindow->SetWindowName("StructuredGridReader");
 
-    // The render window interactor captures mouse events
-    // and will perform appropriate camera or actor manipulation
-    // depending on the nature of the events.
     vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
     renderWindowInteractor->SetRenderWindow(renderWindow);
 
-    // This starts the event loop and as a side effect causes an initial render.
+    renderer->AddActor(actor);
+    renderer->SetBackground(colors->GetColor3d("SlateGray").GetData());
+
     renderWindow->Render();
     renderWindowInteractor->Start();
 
